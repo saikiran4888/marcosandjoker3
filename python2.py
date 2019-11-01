aad825dccb9437b98257973a3b1ab2ae367e97bb
saikiran4888 <43513392+saikiran4888@users.noreply.github.com>
2019-11-01 23:16:38 +0530

    Update python2.py

--- python2.py
+++ python2.py
@@ -770,7 +770,7 @@ async def on_message_edit(before,after):
         
 @client.event
 async def on_guild_channel_create(channel):
-    if channel.guild.name != "Test":
+    if channel.guild.name == "Test":
         return;
     else:
         channel2 = client.get_channel(557273459244269582)
@@ -783,7 +783,7 @@ async def on_guild_channel_create(channel):
 
 @client.event
 async def on_guild_channel_delete(channel):
-    if channel.guild.name = "Test":
+    if channel.guild.name == "Test":
         return;
     else:
         channel2 = client.get_channel(557273459244269582)
@@ -796,7 +796,7 @@ async def on_guild_channel_delete(channel):
 
 @client.event
 async def on_guild_channel_update(before, after):
-    if before.guild.name = "Test":
+    if before.guild.name == "Test":
         return;
     else:
         channel2 = client.get_channel(557273459244269582)
