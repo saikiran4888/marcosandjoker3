9e9c79583ea1dc53f84c59266fb5504a06998def
saikiran4888 <43513392+saikiran4888@users.noreply.github.com>
2021-06-01 21:12:20 +0530

    Update covid-19.py

--- cogs/covid-19.py
+++ cogs/covid-19.py
@@ -11,6 +11,7 @@ import os
 import functools
 import time
 import datetime
+from datetime import datetime as datetime1
 import requests
 import json
 import aiohttp
@@ -93,7 +94,7 @@ class Covid(commands.Cog):
         recovered_cases = (data['recovered']/data['cases'])*100
         death_percentage = (data['deaths']/data['cases'])*100
         lastupdatedraw = data['updated']
-        """lastupdated = datetime.fromtimestamp(lastupdatedraw).strftime("%d-%m-%Y")"""
+        lastupdated = datetime1.fromtimestamp(lastupdatedraw).strftime("%d-%m-%Y")
         embed.add_field(name="Continent", value= data['continent'])
         embed.add_field(name="Population", value=format_number(data['population'], locale="en_IN"))
         embed.add_field(name="Confirmed Cases So Far", value= format_number(data['cases'], locale='en_IN'))
@@ -115,7 +116,7 @@ class Covid(commands.Cog):
         embed.set_thumbnail(url=data['countryInfo']['flag'])
         embed.add_field(name="Tests Done So Far", value= format_number(data['tests'], locale='en_IN'))
         embed.add_field(name="Tests Done Per Million", value= format_number(data['testsPerOneMillion'], locale='en_IN'))
-        """embed.add_field(name="Last Updated", value= lastupdated)"""
+        embed.add_field(name="Last Updated", value= lastupdated)
         embed.timestamp = datetime.datetime.utcnow()
         embed.set_footer(text=f"Requested by {ctx.message.author}", icon_url=ctx.message.author.avatar_url)
         await ctx.send(embed=embed)
